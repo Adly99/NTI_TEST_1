af3ceb8cb65c1aeab344504315af118bbe4fb061
abdelrhman elsayed <103580861+Adly99@users.noreply.github.com>
2023-02-23 22:46:06 +0200

    Initial commit

---
+++ NTI_TEST_1/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\BCD\seven segment bcd.c
+
+HAL\Button\button.c
+
+HAL\KEYPAD\keypad.c
+
+HAL\LCD\lcd.c
+
+HAL\LCD\lcd_cfg.c
+
+HAL\LED\led.c
+
+main.c
+
+MCAL\DIO\dio.c
+
